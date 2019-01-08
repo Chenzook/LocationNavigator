7601171e869892cbaf71d624854c06327b18781d
Seyed Mojtaba Hosseini Zeidabadi <hosseini@chenzook.com>
2019-01-08 13:55:14 +0330

    0.6.9

--- ChenzookLocationNavigator.podspec
+++ ChenzookLocationNavigator.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'ChenzookLocationNavigator'
-  s.version          = '0.6.8'
+  s.version          = '0.6.9'
   s.summary          = 'Let users choose their favorite navigator app.'
 
 # This description is used to generate tags and improve search results.
---
+++ ChenzookLocationNavigator/Classes/UIViewExtension.swift
@@ -0,0 +1,26 @@
+//
+//  UIViewExtension.swift
+//  ChenzookLocationNavigator
+//
+//  Created by Seyed Mojtaba Hosseini Zeidabadi on 1/5/19.
+//  
+//
+//  Linkedin: https://linkedin.com/in/MojtabaHosseini
+//  GitHub: https://github.com/MojtabaHs
+//  Web: https://chenzook.com
+//
+
+import UIKit
+
+extension UIView {
+    var parentViewController: UIViewController? {
+        var parentResponder: UIResponder? = self
+        while parentResponder != nil {
+            parentResponder = parentResponder!.next
+            if let viewController = parentResponder as? UIViewController {
+                return viewController
+            }
+        }
+        return nil
+    }
+}
